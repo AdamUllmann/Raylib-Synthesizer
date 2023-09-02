8c72d3e601ec3be7ea82d2aff6723381462b5dd3
adamullmann <adamullmannbusiness@gmail.com>
2023-09-02 15:24:28 -0700

    Implemented a biquad filter, cleaned/refactored code slightly, and added user resonance controls. Additionally, modified the background color for the oscilloscope.

--- Synth/Synth.cpp
+++ Synth/Synth.cpp
@@ -7,15 +7,18 @@
 #include <unordered_map>
 #include <string>
 #include <set>
-#include <iostream>
 
+// Constants
 const int sampleRate = 44100;
 const float twoPi = 2 * PI;
 const int defaultBufferSize = 1024;
 
+// Global variables
+std::set<float> activeFrequencies;           // core
+std::unordered_map<float, float> phases;    //  core
 float tuningFactor = 1.0f;
-std::set<float> activeFrequencies;
-std::unordered_map<float, float> phases;
+std::vector<std::string> waveTypes = { "sine", "saw", "square", "triangle", "noise" };
+int currentWaveTypeIndex = 0;
 
 // UI elements
 Rectangle cycleWaveformButton = { 10, 10, 150, 40 };
@@ -25,38 +28,51 @@ Rectangle tuneKnob = { 10, 160, 40, 40 };
 
 // filter
 float cutoff = 1000.0f; 
-float resonance = 2.0f;
+float resonance = 1.0f;
 
-class StateVariableFilter {
+class Biquad {
 private:
-    float fs; // sample rate
-    float f;  // frequency
-    float q;  // resonance
-
-    float low, band, high; // filter states
+    float a0, a1, a2, b0, b1, b2;
+    float x1, x2, y1, y2;
 
 public:
-    StateVariableFilter(float sampleRate) : fs(sampleRate), f(440.0), q(1.0), low(0), band(0), high(0) {}
+    Biquad() : a0(0), a1(0), a2(0), b0(1), b1(0), b2(0), x1(0), x2(0), y1(0), y2(0) {}
 
-    void setCutoff(float cutoff) {
-        if (cutoff > 5500.0f) cutoff = 5500.0f;
-        if (cutoff < 0.0f) cutoff = 0.0f;
-        f = cutoff;
+    void setLowPass(float sampleRate, float cutoff, float Q) {
+        float w0 = 2.0f * PI * cutoff / sampleRate;
+        float alpha = sin(w0) / (2.0f * Q);
+        float cosw0 = cos(w0);
+
+        b0 = (1.0f - cosw0) / 2.0f;
+        b1 = 1.0f - cosw0;
+        b2 = b0;
+        a0 = 1.0f + alpha;
+        a1 = -2.0f * cosw0;
+        a2 = 1.0f - alpha;
+
+        b0 /= a0;   // normalize
+        b1 /= a0;
+        b2 /= a0;
+        a1 /= a0;
+        a2 /= a0;
     }
-    void setResonance(float resonance) { q = resonance; }
 
-    float process(float input) {
-        float F = 2.0 * sin((PI * f) / fs);
-        low += F * band;
-        high = input - low - q * band;
-        band += F * high;
-        return low; // returns low-pass. can be changed to band for band-pass and high for high-pass
+    float process(float in) {
+        float out = (b0 / a0) * in + (b1 / a0) * x1 + (b2 / a0) * x2 - (a1 / a0) * y1 - (a2 / a0) * y2;
+
+        x2 = x1;
+        x1 = in;
+
+        y2 = y1;
+        y1 = out;
+
+        return out;
     }
 };
 
+Biquad filter;  // Global filter instance
 
-
-std::unordered_map<std::string, float> noteFrequencies = {
+std::unordered_map<std::string, float> noteToFrequency = {
     // octave 3
     {"C3", 130.81f},
     {"C#3", 138.59f},
@@ -120,64 +136,45 @@ std::unordered_map<int, std::string> keyToNote = {
     {KEY_SEVEN, "A#4"}, {KEY_NINE, "C#5"}, {KEY_ZERO, "D#5"}, {KEY_EQUAL, "F#5"}
 };
 
+void updatePhase(float frequency, float& phase) {
+    phase += twoPi * frequency / sampleRate;
+    if (phase >= twoPi) {
+        phase -= twoPi;
+    }
+}
+
+// Wave function definitions
 float sineWave(float frequency, float& phase) {
     float value = sinf(phase);
-    phase += twoPi * frequency / sampleRate;
-    if (phase >= twoPi) { 
-        phase -= twoPi; 
-    }
+    updatePhase(frequency, phase);
     return value;
 }
-
 float sawWave(float frequency, float& phase) {
     float value = 2.0 * (phase / twoPi) - 1.0;
-    phase += twoPi * frequency / sampleRate;
-    if (phase >= twoPi) {
-        phase -= twoPi;
-    }
+    updatePhase(frequency, phase);
     return value;
 }
-
 float squareWave(float frequency, float& phase) {
     float value;
-    if (phase < PI) {
-        value = 1.0f;
-    }
-    else {
-        value = -1.0f;
-    }
-    phase += twoPi * frequency / sampleRate;
-    if (phase >= twoPi) {
-        phase -= twoPi;
-    }
+    if (phase < PI)     value = 1.0f;
+    else                value = -1.0f;
+    updatePhase(frequency, phase);
     return value;
 }
-
 float triangleWave(float frequency, float& phase) {
     float value;
-    if (phase < PI) {
-        value = -1.0f + (2 / PI) * phase;
-    }
-    else {
-        value = 3.0f - (2 / PI) * phase;
-    }
-    phase += twoPi * frequency / sampleRate;
-    if (phase >= twoPi) {
-        phase -= twoPi;
-    }
+    if (phase < PI)     value = -1.0f + (2 / PI) * phase;
+    else                value = 3.0f - (2 / PI) * phase;
+    updatePhase(frequency, phase);
     return value;
 }
-
 float noise(float frequency, float& phase) {
     float value = (float)GetRandomValue(-1, 1);
-    phase += twoPi * frequency / sampleRate;
-    if (phase >= twoPi) {
-        phase -= twoPi;
-    }
+    updatePhase(frequency, phase);
     return value;
 }
 
-/*void resetPhasesForActiveFrequencies() {
+/*void resetPhasesForActiveFrequencies() {      // useful for debugging
     for (float freq : activeFrequencies) {
         phases[freq] = 0.0f;
     }
@@ -203,13 +200,38 @@ void controls() {
     }
 
     for (const auto& pair : keyToNote) {
-        float tunedFreq = noteFrequencies[pair.second];// * tuningFactor;
+        float tunedFreq = noteToFrequency[pair.second];   // * tuningFactor;
         if (IsKeyDown(pair.first) && activeFrequencies.find(tunedFreq) == activeFrequencies.end()) {
             activeFrequencies.insert(tunedFreq);
         }
         else if (IsKeyReleased(pair.first)) {
             activeFrequencies.erase(tunedFreq);
         }
+    }
+
+    const float incrementFactor = 0.05;
+    if (IsKeyPressed(KEY_LEFT_SHIFT) || IsKeyPressed(KEY_RIGHT_SHIFT)) {
+        currentWaveTypeIndex = (currentWaveTypeIndex + 1) % waveTypes.size();
+    }
+    if (IsKeyDown(KEY_GRAVE)) {
+        cutoff -= cutoff * incrementFactor;
+        if (cutoff < 0.5f) cutoff = 0.5f;                   // we do 0.5 because of the cutoff will never reach 0
+        filter.setLowPass(sampleRate, cutoff, resonance);
+    }
+    if (IsKeyDown(KEY_ONE)) {
+        cutoff += cutoff * incrementFactor;
+        if (cutoff > 21000.0f) cutoff = 21000.0f;
+        filter.setLowPass(sampleRate, cutoff, resonance);
+    }
+    if (IsKeyDown(KEY_BACKSLASH)) {
+        resonance -= 0.1f;
+        if (resonance < 0.707f) resonance = 0.707f;         // picked this arbitrarily. its the number you would use to get RMS voltage (and it's used in analog filters).
+        filter.setLowPass(sampleRate, cutoff, resonance);                                                                              // (it means nothing here)
+    }
+    if (IsKeyDown(KEY_BACKSPACE)) {
+        resonance += 0.1f;
+        if (resonance > 100.0f) resonance = 100.0f;
+        filter.setLowPass(sampleRate, cutoff, resonance);
     }
 }
 
@@ -230,21 +252,8 @@ int main() {
     float frequency = 440.0f;
     float phase = 0.0f;
 
-    StateVariableFilter svf(sampleRate);
-    svf.setCutoff(1000.0);
-    svf.setResonance(2.0);
-    StateVariableFilter svf1(sampleRate);   // for 4-pole
-    svf1.setCutoff(1000.0);
-    svf1.setResonance(2.0);
-    StateVariableFilter svf2(sampleRate);   // for 6-pole
-    svf2.setCutoff(1000.0);
-    svf2.setResonance(2.0);
-
-    std::string wave = "sine";
-
-    std::vector<std::string> waveTypes = { "sine", "saw", "square", "triangle", "noise"};
-    int currentWaveTypeIndex = 0;
-
+    filter.setLowPass(sampleRate, cutoff, resonance);
+    
     std::vector<short> buffer(defaultBufferSize);
 
     SetTargetFPS(60);
@@ -256,7 +265,7 @@ int main() {
                     float sampleValue = 0.0f;
                     for (float freq : activeFrequencies) {
                         float tunedFreq = freq * tuningFactor;
-                        double phaseIncrement = twoPi * tunedFreq / sampleRate;
+                        float phaseIncrement = twoPi * tunedFreq / sampleRate;
                         if (waveTypes[currentWaveTypeIndex] == "sine") {
                             sampleValue += sineWave(tunedFreq, phases[freq]);
                         }
@@ -280,12 +289,11 @@ int main() {
                     }
                     sampleValue /= activeFrequencies.size() > 0 ? activeFrequencies.size() : 1;
 
-                    sampleValue = svf.process(sampleValue);
-
-                     sampleValue = svf1.process(sampleValue);
-                     //sampleValue = svf2.process(sampleValue);
-
+                    sampleValue = filter.process(sampleValue);
                     sampleValue *= 0.5;
+                    if (sampleValue > 1.0f) sampleValue = 1.0f;
+                    if (sampleValue < -1.0f) sampleValue = -1.0f;
+
                     buffer[i] = static_cast<short>(sampleValue * INT16_MAX);
 
                     circularBuffer[bufferIndex] = buffer[i];
@@ -295,24 +303,7 @@ int main() {
             }
         }
         controls();
-
-        if (IsKeyPressed(KEY_LEFT_SHIFT) || IsKeyPressed(KEY_RIGHT_SHIFT)) {
-            currentWaveTypeIndex = (currentWaveTypeIndex + 1) % waveTypes.size();
-        }
-        if (IsKeyDown(KEY_GRAVE)) {
-            cutoff -= 100.0;
-            svf.setCutoff(cutoff);
-            svf1.setCutoff(cutoff);
-            svf2.setCutoff(cutoff);
-            std::cout << cutoff << std::endl;
-        }
-        if (IsKeyDown(KEY_ONE)) {
-            cutoff += 100.0;
-            svf.setCutoff(cutoff);
-            svf1.setCutoff(cutoff);
-            svf2.setCutoff(cutoff);
-            std::cout << cutoff << std::endl;
-        }
+        
 
         /*if (CheckCollisionPointRec(GetMousePosition(), cycleWaveformButton) && IsMouseButtonReleased(MOUSE_LEFT_BUTTON)) {
             currentWaveTypeIndex = (currentWaveTypeIndex + 1) % waveTypes.size();
@@ -329,9 +320,9 @@ int main() {
 
 
         BeginDrawing();
-        ClearBackground(RAYWHITE);
+        ClearBackground(BLACK);
 
-       for (int i = 1; i < oscilloscopeSize; i++) {
+       for (int i = 1; i < oscilloscopeSize; i++) {                             // oscilloscope stuff
             int prevIndex = (bufferIndex + i - 1) % oscilloscopeSize;
             int currentIndex = (bufferIndex + i) % oscilloscopeSize;
 
